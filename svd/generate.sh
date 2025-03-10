465e45cc3bee2042d1a72bf71df159108ab54045
Vadim Godunko <vgodunko@gmail.com>
2025-03-10 18:27:31 +0400

    Change name of the root package.

--- svd/generate.sh
+++ svd/generate.sh
@@ -2,7 +2,7 @@
 
 svd2ada STM32G474.svd \
   --output ../source/svd \
-  --package A0B.STM32G4XX.SVD \
+  --package A0B.STM32G4.G474.SVD \
   --base-types-package A0B.Types.SVD \
   --boolean \
   --no-elaboration-code-all \
